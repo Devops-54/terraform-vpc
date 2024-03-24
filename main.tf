c46f246d2d7ee1db81af566b2e29cfc6a33c2456
Uttej uv <sushprav11@gmail.com>
2024-03-24 15:11:02 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
-module "vpc" {
-  source                = "./vendor/modules"
+module "VPC" {
+
+  source                = "./vendor/modules/vpc"
   VPC_CIDR              = var.VPC_CIDR
   ENV                   = var.ENV
   AZ                    = var.AZ
